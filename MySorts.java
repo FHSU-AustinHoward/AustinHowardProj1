6067b11dd9311e676cd63fcd0d26f7bf4ee93976
FHSU-AustinHoward <A_Howard4@mail.fhsu.edu>
2024-06-09 10:23:57 -0500

    Created MySorts Class and added basic structure

---
+++ MySorts.java
@@ -0,0 +1,51 @@
+/**
+ * CSCI251ProjOne. This is the class definition for Project One
+ * This class is turned in for the assignment
+ *
+ * @author Austin Howard
+ * @version Jun 9, 2023
+ */
+
+public class MySorts {
+
+    public static void insertSort(int[] arr) {
+
+    }
+
+    public static void selectSort(int[] arr) {
+
+    }
+
+    public static void quickSort(int[] arr) {
+
+    }
+
+    public static void mergeSort(int[] arr) {
+
+    }
+
+    // merge method
+    // merge two sorted	portions of	given array	arr, namely, from start	to middle
+    // and from	middle + 1 to end into one sorted portion, namely, from	start to end
+    private static void merge(int[] arr, int start, int middle, int end) {
+
+    }
+
+    // merge sort recursive version
+    // sort	the	portion	of giving array	arr, from begin	to end
+    private static void mergeSortRecursive(int[] arr, int begin, int end) {
+
+    }
+
+    // find	pivot point	for	quick sort
+    private static int pivot(int[] arr, int begin, int end) {
+        return 0;
+    }
+
+    // quick sort recursive	version
+    // sort	the	portion	of giving array	arr, from begin	to end
+    private static void quickSortRecursive(int[] arr, int begin, int end) {
+
+    }
+
+}
